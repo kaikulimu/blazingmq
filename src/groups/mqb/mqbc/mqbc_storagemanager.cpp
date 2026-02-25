859a75bff274c03d811d83d91950d5021cf53e43
Yuan Jing Vincent Yan <yyan82@bloomberg.net>
2026-02-25 14:42:12 -0500

    Formatting
    Signed-off-by: Yuan Jing Vincent Yan <yyan82@bloomberg.net>

--- src/groups/mqb/mqbc/mqbc_storagemanager.cpp
+++ src/groups/mqb/mqbc/mqbc_storagemanager.cpp
@@ -600,8 +600,7 @@ void StorageManager::processReplicaDataRequestPull(
 
         BALL_LOG_ERROR
             << d_clusterData_p->identity().description() << " Partition ["
-            << partitionId << "]: "
-            << "Received ReplicaDataRequestPull from "
+            << partitionId << "]: " << "Received ReplicaDataRequestPull from "
             << source->nodeDescription() << " with endSequenceNumber: "
             << replicaDataRequest.endSequenceNumber()
             << " that does not match self's current sequence number: "
@@ -2032,8 +2031,8 @@ void StorageManager::do_determineDataDestinations(const EventWithData& event)
                 selfFirstSyncAfterRolloverSeqNum) {
             BALL_LOG_WARN
                 << d_clusterData_p->identity().description() << " Partition ["
-                << partitionId << "]: "
-                << "Replica " << cit->first->nodeDescription()
+                << partitionId << "]: " << "Replica "
+                << cit->first->nodeDescription()
                 << " has non-empty storage with different first sync point, "
                 << "implying that it has missed rollover.  We need to send "
                 << "ReplicaDataRequestDrop to this replica.";
@@ -2054,8 +2053,8 @@ void StorageManager::do_determineDataDestinations(const EventWithData& event)
 
             BALL_LOG_WARN
                 << d_clusterData_p->identity().description() << " Partition ["
-                << partitionId << "]: "
-                << "Replica " << cit->first->nodeDescription()
+                << partitionId << "]: " << "Replica "
+                << cit->first->nodeDescription()
                 << " has partition sequence number " << nodeSeqNum
                 << ", while the historic highest sequence number for the same "
                 << "primary lease ID is " << historicHighestSeqNum
@@ -2073,8 +2072,8 @@ void StorageManager::do_determineDataDestinations(const EventWithData& event)
             // replica.
             BALL_LOG_INFO
                 << d_clusterData_p->identity().description() << " Partition ["
-                << partitionId << "]: "
-                << "Replica " << cit->first->nodeDescription()
+                << partitionId << "]: " << "Replica "
+                << cit->first->nodeDescription()
                 << " has partition sequence number " << nodeSeqNum
                 << ", which is lower than or equal to self's partition "
                 << "sequence number " << selfSeqNum
@@ -2086,8 +2085,8 @@ void StorageManager::do_determineDataDestinations(const EventWithData& event)
         else {
             BALL_LOG_WARN
                 << d_clusterData_p->identity().description() << " Partition ["
-                << partitionId << "]: "
-                << "Replica " << cit->first->nodeDescription()
+                << partitionId << "]: " << "Replica "
+                << cit->first->nodeDescription()
                 << " has partition sequence number " << nodeSeqNum
                 << ", which is higher than self's partition sequence number "
                 << selfSeqNum
