e972740b411341e6ef7260997b2eb35a2e565237
Yuan Jing Vincent Yan <yyan82@bloomberg.net>
2026-04-14 16:15:54 -0400

    mqbs::FileStore: Update highestSeqNums when bumping lease id
    Signed-off-by: Yuan Jing Vincent Yan <yyan82@bloomberg.net>

--- src/groups/mqb/mqbs/mqbs_filestore.cpp
+++ src/groups/mqb/mqbs/mqbs_filestore.cpp
@@ -6356,6 +6356,10 @@ void FileStore::processStorageEvent(const bsl::shared_ptr<bdlbb::Blob>& blob,
                 // get a separate notification about leaseId (unlike in steady
                 // state when StorageMgr invokes fs.setActivePrimary()).
 
+                if (d_primaryLeaseId > 0 &&
+                    recHeader->primaryLeaseId() > d_primaryLeaseId) {
+                    d_highestSeqNums[d_primaryLeaseId] = d_sequenceNum;
+                }
                 d_primaryLeaseId = recHeader->primaryLeaseId();
 
                 BALL_LOG_INFO
@@ -6483,6 +6487,9 @@ int FileStore::processRecoveryEvent(const bsl::shared_ptr<bdlbb::Blob>& blob)
             else if (d_primaryLeaseId < recHeader->primaryLeaseId()) {
                 // LeaseId was bumped up.
 
+                if (d_primaryLeaseId > 0) {
+                    d_highestSeqNums[d_primaryLeaseId] = d_sequenceNum;
+                }
                 d_primaryLeaseId = recHeader->primaryLeaseId();
                 d_sequenceNum    = recHeader->sequenceNumber();
             }
@@ -6749,8 +6756,11 @@ void FileStore::setActivePrimary(mqbnet::ClusterNode* primaryNode,
     }
 
     if (primaryLeaseId > d_primaryLeaseId) {
-        // Reset the sequence number only if primary leaseId has been bumped
-        // up.
+        // Record the highest sequence number for the previous primary lease id
+        // before bumping.
+        if (d_primaryLeaseId > 0) {
+            d_highestSeqNums[d_primaryLeaseId] = d_sequenceNum;
+        }
 
         d_sequenceNum = 0;
     }
